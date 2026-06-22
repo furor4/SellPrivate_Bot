0a8bba6a13574fc9012af07432fef7dabf5a1125
furor4 <m_yevtushenko@list.ru>
2026-06-22 12:06:04 +0300

    убрал лишние комментарии

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ env = Env()
 env.read_env('.env')
 
 TOKEN = env.str('TOKEN')
-OWNER_ID = env.int('ADMIN')  # Владелец приватки
-CHANNEL_ID = env.int('PRIVATE_CHANNEL_ID')  # Айди канала приватки
-CHAT_ID = env.int('PRIVATE_CHAT_ID')  # Айди чата приватки (если существует)
+OWNER_ID = env.int('ADMIN')
+CHANNEL_ID = env.int('PRIVATE_CHANNEL_ID')
+CHAT_ID = env.int('PRIVATE_CHAT_ID')
 DATABASE_URL = env.str('DATABASE_URL')
 
 MSK = pytz.timezone('Europe/Moscow')  # Время по МСК
