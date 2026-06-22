fcc7b9455a180492b62fe4bf55efa4b15a617a06
furor4 <m_yevtushenko@list.ru>
2026-06-22 12:08:06 +0300

    добавил данные для базы данных

--- config.py
+++ config.py
@@ -10,7 +10,12 @@ TOKEN = env.str('TOKEN')
 OWNER_ID = env.int('ADMIN')
 CHANNEL_ID = env.int('PRIVATE_CHANNEL_ID')
 CHAT_ID = env.int('PRIVATE_CHAT_ID')
-DATABASE_URL = env.str('DATABASE_URL')
+
+DB_USER = env.str('DB_USER')
+DB_PASSWORD = env.str('DB_PASSWORD')
+DB_NAME = env.str('DB_NAME')
+
+DATABASE_URL = env.str('DATABASE_URL', f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@127.0.0.1:5432/{DB_NAME}")
 
 MSK = pytz.timezone('Europe/Moscow')  # Время по МСК
 bot = Bot(token=TOKEN)
